d5e513814dd6509ac57b8a173e77cebcd30c2fdd
Martha avila <itit.martha.avil@gmail.com>
2022-10-30 21:02:29 -0600

    esta clase trato sobre el uso de la  reasignación en  variables  var, let y const. de como la reaccignación redeclaración nos permite  modificar  valores dentro de  una variable

---
+++ src/reasignadeclara.js
@@ -0,0 +1,39 @@
+var fristname ; //  aqui declaramos cuando declaramos  este  se asigna undefined por default, ya  que no tiene asignación de valor
+
+var fristname = "Martha"; // aqui esta asignada
+
+console.log(fristname); // mostramos 
+
+
+// asignamos y declarmos 
+
+var lastname = "Elba"; 
+
+//  aqui reasignar
+
+lastname = "pepita"; 
+
+//mostramos
+
+console.log(lastname); 
+
+// con let y const 
+
+let  fullname = "Martha elba"; 
+
+fullname  = "Tomas torres";
+
+console.log(fullname);
+// con let es permitido la reasignación de  valor
+// pero no redeclara 
+
+
+const  namepet = "pinky";
+
+namepet = "cuco";
+
+console.log(namepet); 
+
+// con  const  no puede reasignar valores
+// const nos permite ser mutables en arreglos  y objetos
+
